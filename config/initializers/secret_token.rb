a30a1788655deb9b06f1e2bf351a55170767e040
shufen Chao <shufen@usamp.com>
2013-01-25 10:04:54 -0600

    Initi checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopwithus::Application.config.secret_token = '9469e6f319effea410363428e9a98e1740c3aaf6c597887db915a2e4143af47085eac4fe80218324d61fc720e449597a65999c20457724d6741740b8f49951ba'
